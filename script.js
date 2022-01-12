3fdff42bd6798a0a16a2b3645ae555e2f39fd005
lucenarapereira <93477361+lucenarapereira@users.noreply.github.com>
2022-01-11 23:29:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log('Array: ' + arrayNumeros) 
+
+var arrayInvertido = [];
+arrayInvertido = arrayNumeros.reverse();
+console.log('Array Invertido: ' + arrayInvertido) 
